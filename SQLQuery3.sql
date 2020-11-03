4d6d9ea88f1b7681f1e08016c995635ead533185
Senendra <senendra.007@gmail.com>
2020-11-03 07:29:42 +0530

    [Senendra] Add: Inserting the values in table AddressBook

---
+++ SQLQuery3.sql
@@ -0,0 +1,4 @@
+insert into AddressBook values
+('Raj','Devakar','Krinshna Nagar','Durg','Chhattisgarh',490025,'9988556611','zxc@gamil.com'),
+('Abhishek','Shrivastav','Mahavir Nagar','Raipur','Chhattisgarh',490022,'9865321245','abc@gmail.com'),
+('Ritu','Sahu','Dixit Colony','Bhilai','Chhattisgarh',490023,'7845123265','mki@gamil.com')
