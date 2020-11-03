84961966e736ee61a1c7e8e3d26d0f46dfa20375
Senendra <senendra.007@gmail.com>
2020-11-03 07:18:53 +0530

    [Senendra] Add: Creating table AddressBook

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+create table AddressBook
+(
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+address varchar(50) not null,
+city varchar(15),
+state varchar(15),
+zip int,
+phone_number char(10) not null,
+email varchar(30) not null
+);
